4936abcc1eef7f95ae1b7df743da4aa716f9b2d6
Owen Cruse <ocruse7@gmail.com>
2019-10-24 17:29:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,11 @@
 def new_hash
-  
+  hash1 = Hash.new
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  my_hash = {
+    "hello" => "there"
+  }
 end
 
 def pioneer
